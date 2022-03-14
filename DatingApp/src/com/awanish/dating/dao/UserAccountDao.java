22f98804daa15b84e4cedab6f8dc7d76a98ad660
awanish285 <45520212+awanish285@users.noreply.github.com>
2022-03-15 03:26:36 +0530

    Updating UseDao file.

--- DatingApp/src/com/awanish/dating/dao/UserAccountDao.java
+++ DatingApp/src/com/awanish/dating/dao/UserAccountDao.java
@@ -6,15 +6,15 @@ import com.awanish.dating.model.UserAccount;
 
 //UserAccount Dao class all method implements by Impl class
 public interface UserAccountDao {
-	
+	//This method is used to save UserAccount
 	void save(UserAccount userAccount);
-	
+	//This method is used to find user by id
 	UserAccount findById(int id);
-	
+	//this method is used to finad all the useres
 	List<UserAccount> findAll();
-	
+	//this method is used to find the mataches
 	List<UserAccount> findMatchers(int age,String city);
-	
+	//this method is used to delete the userAccount
 	void delete(int id);
 
 }
